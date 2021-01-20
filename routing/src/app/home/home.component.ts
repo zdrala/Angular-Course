9ea8fca69709907e7bcd1ad9831aa18890e96bcb
Ahmed Ždralović <ahmed.zdralovic@edu.fit.ba>
2021-01-20 19:46:09 +0100

    routerLinks,navigating programmatically,passing route parameters,passing query parameters,redirecting and Wildcard routes,canActivate Guard, canActivateChild Guard, canDeactivate Guard, passing static data to a route, resolving dynamic data with the resolve guard

---
+++ routing/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private router:Router, private authservice:AuthService) { }
+
+  ngOnInit() {
+  }
+  onLoadServers(id:number){  
+    this.router.navigate(['/servers',id,'edit'],{queryParams:{allowEdit:'1'}, fragment:'loading'});
+  }
+  onLogin(){
+    this.authservice.login();
+  }
+  onLogout(){
+    this.authservice.logout();
+  }
+}
